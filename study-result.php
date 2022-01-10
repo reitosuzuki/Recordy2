c532edb6fbb77d08a2e9e9b72d0a0dd8815f6dc3
reitosuzuki <reitosuzuki.shohei@gmail.com>
2022-01-10 14:21:38 +0900

    Update study-result.php

--- study-result.php
+++ study-result.php
@@ -14,13 +14,12 @@ if (isset($_SESSION['customer']['id'])) {
   $pdo=new PDO('mysql:host=localhost;dbname=recordy;charset=utf8',
     'staff', 'reito417');
   $customer_id=$_SESSION['customer']['id'];
-  foreach ($pdo->query('select to_do_1,to_do_2,to_do_3 from to_do where customer_id=$customer_id and max(created_at) ') as $todo) {
-    echo "$todo";
+  $sql=$pdo->prepare('select to_do_1,to_do_2,to_do_3 from to_do where customer_id=$customer_id and max(created_at) ')
+  $sql->execute([$_REQUEST[]])
   }
   }
  ?>
 
- }
 
 
 
